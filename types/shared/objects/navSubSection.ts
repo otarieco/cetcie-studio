f56773aa1da80c5b43d90dd2a132b2c7641164d8
Jérémy Le Mardelé <jeremy@otarie.co>
2024-02-14 11:09:05 +0100

    feat: add _type property to NavSubSection interface (#3)
    The _type property is added to the NavSubSection interface to specify the type of the object according to the SANITY_FIELDS.NAV_SUB_SECTION schema. This change ensures that the NavSubSection object conforms to the defined schema.

--- types/shared/objects/navSubSection.ts
+++ types/shared/objects/navSubSection.ts
@@ -1,6 +1,8 @@
 import { type Link, LinkProjection } from './link';
+import { SANITY_FIELDS } from '../../sanity.schemas';
 
 export type NavSubSection = {
+  _type: SANITY_FIELDS.NAV_SUB_SECTION;
   title?: string;
   links?: Link[];
 };
