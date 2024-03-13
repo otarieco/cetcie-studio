3483c757ac7ada91bf002e6910fb0d733159baf4
Jérémy Le Mardelé <jeremy@otarie.co>
2024-03-13 23:25:05 +0100

    Adding footer types

--- types/horse/singletons/footer.ts
+++ types/horse/singletons/footer.ts
@@ -1,31 +1,23 @@
 import type {SanityDocument} from 'sanity';
-import {SANITY_FIELDS, SANITY_SINGLETONS} from '../../sanity.schemas';
-import type {Locale} from '../../shared/locale';
-import {type Image, ImageProjection} from '../../shared/objects/image';
-import type {RichTextLite} from '../../shared/objects/richTextLite';
 import {type Link, LinkProjection} from '../../shared/objects/link';
-import {type NavSection, NavSectionProjection} from '../../shared/objects/navSection';
+
+type NavigationGroup = {
+  _type: 'group';
+  _key: string;
+  title?: string;
+  links?: Link[];
+}
 
 export type Footer = SanityDocument & {
-  _type: SANITY_SINGLETONS.$HORSE_FOOTER;
-  locale?: Locale;
-  logo?: Image;
-  description?: RichTextLite;
-  navigationMain?: NavSection[];
-  navigationBottom?: Link[];
+  navigation?: NavigationGroup[];
 };
 
 export const FooterProjection = `
-  ...,
-  logo{${ImageProjection}},
-  navigationMain[]{
-    _type == "${SANITY_FIELDS.NAV_SECTION}" => {
-      ${NavSectionProjection}
-    },
-  },
-  navigationBottom[]{
-    _type == "${SANITY_FIELDS.LINK}" => {
+  navigation[]{
+    ...,
+    links[]{
+      _key,
       ${LinkProjection}
-    },
-  }
+    }
+  },
 `;
